aaecdc3f45339317a6bd002ec888f72734b47ae1
cd <lesleyrotonto@gmail.com>
2023-12-28 13:08:41 -0500

    successfully grab the single recipe id

--- index.js
+++ index.js
@@ -3,28 +3,43 @@
 //get the user by id and their entrie recipe history
 
 let recipes = []
+let oneRecipe = []
 let users = []
 
 
 document.addEventListener('DOMContentLoaded', function(){
     allRecipes()
     .then(()=>{
-        return render()
+        return renderRecipes()
     })
 })
+
+// document.addEventListener('DOMContentLoaded', function(){
+//     recipeByID()
+//     .then(()=>{
+
+//     })
+// })
+
 
 async function allRecipes(){
     const response = await axios.get("https://heartfeltbites-3a2e21beb448.herokuapp.com/api/recipes")
     .then(response=>{
-        console.log(response.data.allRecipes)
         recipes = response.data.allRecipes
     })
     
 }
 
-function render(){
+async function recipeByID(id){
+    const response = await axios.get(`https://heartfeltbites-3a2e21beb448.herokuapp.com/api/recipes/${id}`)
+    .then(response=>{
+        oneRecipe = response.data.singleRecipe
+    })
+}
+
+function renderRecipes(){
     recipes.forEach((recipe) => {
-        const allRecipesContainer = document.querySelector('.allRecipes-containter')
+        const allRecipesContainer = document.querySelector('.allRecipes-container')
 
         //create the divs where the information will live
         const recipeDiv = document.createElement('div')
@@ -32,11 +47,15 @@ function render(){
         //will comeback and add image somehow 
 
         //make titles a hyperlink
-        recipeTitle.href = ""
+        recipeTitle.href = "html/recipePage.html"
 
         //give class name 
         recipeDiv.className = "recipe"
-        recipeTitle.className = "title"
+        recipeTitle.className = "singleTitle"
+
+        //set ID 
+        recipeTitle.setAttribute('data-id', recipe._id)
+        
         
         //fill text
         recipeTitle.textContent = recipe.title
@@ -46,6 +65,13 @@ function render(){
 
         //append div to page
         allRecipesContainer.appendChild(recipeDiv)
+
+        recipeTitle.addEventListener('click', function(e){
+            // e.preventDefault()
+            const id = e.target.getAttribute('data-id')//grabs the id for the recipe you click on 
+            console.log('clicked recipe id:',id)
+            recipeByID(id)
+        })
 
     });
 }
