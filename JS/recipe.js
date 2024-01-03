53d14c9239e867a5473c1f1a086f3becd7fc3d48
cd <lesleyrotonto@gmail.com>
2024-01-03 18:12:33 -0500

    edit button allows to edit and save and sends it to db

--- JS/recipe.js
+++ JS/recipe.js
@@ -52,12 +52,6 @@ async function allRecipes(){
         console.error("Error fetching all recipes: ", error.response.data || error.message);
     }
     
-    // .then(response=>{
-
-    //     //stores the fetched data in an array
-    //     recipes = response.data.allRecipes
-    // })
-    
 }
 
 //needs a recipe ID as a parameter with an async get with the ID as a parameter
@@ -66,20 +60,67 @@ async function recipeByID(id){
     try {
         const response = await axios.get(`http://localhost:3000/api/recipes/${id}`, {headers: {Authorization: `Bearer ${token}`}}) 
         oneRecipe = response.data.singleRecipe
+        // console.log(oneRecipe)
     } catch (error) {
         console.error(error)
     }
-    // const response = await axios.get(`https://heartfeltbites-3a2e21beb448.herokuapp.com/api/recipes/${id}`)
-    // .then(response=>{
-
-    //     //stores the fetched recipe in the oneRecipe that was globaly declared
-    //     oneRecipe = response.data.singleRecipe
-    // }).catch((error)=>{
-    //     console.error(error)
-    // })
 }
 
+async function editRecipe(e, id){
+    //get the token from local storage
+    const token = localStorage.getItem('token')
+    //target the parent node holding the area you want to edit
+    const recipeDiv = e.target.parentNode 
+
+    //target the area you want to edit
+    const recipeImage = recipeDiv.querySelector('.recipeImg')
+    const recipeTitle = recipeDiv.querySelector('.recipeTitle')
+    const recipeIngredients = recipeDiv.querySelector('.recipeIngredients')
+    const recipeDirections = recipeDiv.querySelector('.recipeDirections')
+    const recipeStory = recipeDiv.querySelector('.recipeStory')
+
+    //because there are two buttons on the page you need to target the one that was clicked
+    const editBtn = recipeDiv.querySelector('button:nth-of-type(1)')
+
+    //if the button says edit you will allow the click to make the area editable and change the edit button to say save
+    if(editBtn.textContent === 'Edit'){
+        recipeImage.contentEditable = true
+        recipeTitle.contentEditable = true
+        recipeIngredients.contentEditable = true
+        recipeDirections.contentEditable = true
+        recipeStory.contentEditable = true
+
+        //change the button to say save
+        editBtn.textContent = 'Save Changes'
+    }else{
+        recipeImage.contentEditable = false
+        recipeTitle.contentEditable = false
+        recipeIngredients.contentEditable = false
+        recipeDirections.contentEditable = false
+        recipeStory.contentEditable = false
+
+        //change the button to say edit
+        editBtn.textContent = 'Edit'
+
+        //grab the new information from the page
+        const newImage = recipeImage.textContent
+        const newTitle = recipeTitle.textContent
+        const newIngredients = recipeIngredients.textContent
+        const newDirections = recipeDirections.textContent
+        const newStory = recipeStory.textContent
+
+        try {
+            const response = await axios.put(`http://localhost:3000/api/recipes/${id}`, {imagePath: newImage, title: newTitle, ingredients: newIngredients, directions: newDirections, story: newStory}, {headers: {Authorization: `Bearer ${token}`}})
+        } catch (error) {
+           throw new Error(error) 
+        }
+    }
+}
+
+
 function renderSingle(){
+    //adding decodeJWT to get the user id from the token
+    const userId = decodeJWT().payload.userId
     // checks if oneRecipe is not empty 
     if(oneRecipe){ 
         //find the section in the html made for this area
@@ -112,7 +153,7 @@ function renderSingle(){
 
             recipeImg.src = oneRecipe.imagePath
             recipeTitle.textContent = oneRecipe.title
-            recipeUser.textContent = oneRecipe.user
+            recipeUser.textContent = oneRecipe.user.username
             recipeCreated.textContent = oneRecipe.timestamp
             recipeIngredients.textContent = oneRecipe.ingredients
             recipeDirections.textContent = oneRecipe.recipeDirections
@@ -129,9 +170,27 @@ function renderSingle(){
     
             //append the div to the html page
             singleRecipeContainer.appendChild(recipeDiv)
-        }
-}
 
+            if(userId === oneRecipe.user._id){
+                const editBtn = document.createElement('button')
+                editBtn.className = 'editBtn'
+                editBtn.textContent = 'Edit'
+                recipeDiv.appendChild(editBtn)
+                const deleteBtn = document.createElement('button')
+                deleteBtn.className = 'deleteBtn'
+                deleteBtn.textContent = 'Delete'
+                recipeDiv.appendChild(deleteBtn)
+                
+                editBtn.addEventListener('click', function(e){
+                    editRecipe(e, oneRecipe._id)
+                })
+
+                deleteBtn.addEventListener('click', async function(e){
+                    
+                })
+            } 
+        }
+    }
 
 function renderRecipes(){
     //iterates over the recipes array
@@ -169,4 +228,25 @@ function renderRecipes(){
         })
 
     });
+}
+
+//I need to get the user id from the token so that i can see if the user is on the single recipe page to allow edits
+function decodeJWT() {
+    const token = localStorage.getItem('token'); // Retrieve the token from local storage
+    if (!token) {
+        throw new Error('No token found in local storage');
+    }
+
+    const parts = token.split('.'); // Split the token into parts
+    if (parts.length !== 3) {
+        throw new Error('The token is invalid');
+    }
+
+    const decodedHeader = atob(parts[0]); // Decode the header
+    const decodedPayload = atob(parts[1]); // Decode the payload
+
+    return {
+        header: JSON.parse(decodedHeader),
+        payload: JSON.parse(decodedPayload)
+    };
 }
